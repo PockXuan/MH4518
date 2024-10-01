ce2d4cd4ebfa5a04057c7c8603c6bbff7a60b5d5
macsdude <wesleylow00@gmail.com>
2024-10-01 20:45:27 +0800

    Fully debugged calibrator (hopefully)

--- heston_model.py
+++ heston_model.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-from torchimize.functions import lsq_lma
+import torch.func as F
 from tqdm import tqdm
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -23,38 +23,33 @@ class HestonModel:
         }
         
         # Initial values from the paper [V0,theta,rho,kappa,sigma]
-        self.params = torch.tensor([1.2, 0.2, 0.3, -0.6, 0.2]).to(device)
+        self.params = torch.tensor([[1.2], 
+                                    [0.2], 
+                                    [0.3], 
+                                    [-0.6], 
+                                    [0.2]], requires_grad=True).to(device)
+        
+        self.quad = {'u': None,
+                     'x': None}
     
     def pricing(self, parameters, market_data, u, w):
         
-        num_nodes = u.shape[0]
+        num_nodes = int(u.shape[0] / 2)
         
-        V0 = parameters[0]
-        theta = parameters[1]
-        rho = parameters[2]
-        kappa = parameters[3]
-        sigma = parameters[4]
+        V0, theta, rho, kappa, sigma = parameters
         
         K = market_data['K']
         T = market_data['T']
         S0 = market_data['S0']
         r = market_data['r']
-        
-        # From the paper, their code uses 200 as an upper bound with 64 nodes
-        u_max = 200
-        u = 0.5 * u_max * (u + 1) # Rescaling to quadrature domain
-        u = torch.cat((u,-u), dim=1)
-        u = torch.cat((u-1j,u), dim=1)
 
         # Required constants for the characteristic function
         xi = kappa - sigma * rho * u * 1j
         d = torch.sqrt(xi**2 + sigma**2 * u * (u + 1j))
-        
-        A1 = u * (u + 1j) * torch.sinh(d * T/ 2)
+        A1 = u * (u + 1j) * torch.sinh(d * T / 2)
         A2 = d * torch.cosh(d * T/ 2) + xi * torch.sinh(d * T/ 2)
         A = A1 / A2
         D = torch.log(d) + (kappa - d) * T/ 2 - torch.log((d + xi) / 2 + (d - xi) * torch.exp(-d * T) / 2)
-        B = torch.exp(D)
         
         # Characteristic function
         char_func = torch.exp(1j * u * (torch.log(S0) + r * T) - T * kappa * theta  * rho * u * 1j / sigma - V0 * A + 2 * kappa * theta * D / sigma**2)
@@ -62,40 +57,34 @@ class HestonModel:
         offset = 0.5 * (market_data['S0'] - torch.exp(-market_data['r'] * market_data['T']) * market_data['K'])
         
         # Gauss-Legendre quadrature
-        integrand = torch.real((K**(-u * 1j) / (u * 1j)).unsqueeze(-1) * char_func)
-        integrand = integrand[:2*num_nodes,:] - K.unsqueeze(-1) * integrand[2*num_nodes:,:]
-        integrand = w.reshape(-1,1) * 0.5 * u_max * (integrand[:num_nodes,:] + integrand[num_nodes:,:])
-        integrand = torch.sum(integrand, dim=0) * torch.exp(-r * T.reshape(-1,1)) / np.pi
-        
-        return offset + integrand
+        integrand = torch.real((K**(-u * 1j) / (u * 1j)) * char_func)
+        integrand = integrand[:num_nodes,:] - K * integrand[num_nodes:,:]
+        integrand = w.reshape(-1,1) * (integrand[:num_nodes//2,:] + integrand[num_nodes//2:,:])
+        integrand = torch.sum(integrand, dim=0) * torch.exp(-r * T.reshape(-1,1)).reshape(1, -1) / np.pi
+    
+        return (offset + integrand).T
         
     def jacobian(self, parameters, market_data, u, w):
         
-        num_nodes = u.shape[0]
+        num_nodes = int(u.shape[0] / 2)
         
-        V0 = parameters[0]
-        theta = parameters[1]
-        rho = parameters[2]
-        kappa = parameters[3]
-        sigma = parameters[4]
+        V0, theta, rho, kappa, sigma = parameters
+        V0.retain_grad()
+        theta.retain_grad()
+        rho.retain_grad()
+        kappa.retain_grad()
+        sigma.retain_grad()
         
         K = market_data['K']
         T = market_data['T']
         S0 = market_data['S0']
         r = market_data['r']
-        
-        # From the paper, their code uses 200 as an upper bound with 64 nodes
-        u_max = 200
-        u = 0.5 * u_max * (u + 1) # Rescaling to quadrature domain
-        u = torch.cat((u,-u), dim=1)
-        u = torch.cat((u-1j,u), dim=1)
 
         # Required constants for the characteristic function
         xi = kappa - sigma * rho * u * 1j
         d = torch.sqrt(xi**2 + sigma**2 * u * (u + 1j))
-        
-        A1 = u * (u + 1j) * torch.sinh(d * T/ 2)
-        A2 = d * torch.cosh(d * T/ 2) + xi * torch.sinh(d * T/ 2)
+        A1 = u * (u + 1j) * torch.sinh(d * T / 2)
+        A2 = d * torch.cosh(d * T / 2) + xi * torch.sinh(d * T / 2)
         A = A1 / A2
         D = torch.log(d) + (kappa - d) * T/ 2 - torch.log((d + xi) / 2 + (d - xi) * torch.exp(-d * T) / 2)
         B = torch.exp(D)
@@ -103,27 +92,38 @@ class HestonModel:
         # Characteristic function
         char_func = torch.exp(1j * u * (torch.log(S0) + r * T) - T * kappa * theta  * rho * u * 1j / sigma - V0 * A + 2 * kappa * theta * D / sigma**2)
         
-        d.backward()
-        dd_drho = rho.grad.clone()
-        rho.grad.zero_()
-        dd_dsigma = sigma.grad.clone()
-        sigma.grad.zero_()
+        # Because why is there no easy way to do this...
+        def d_func(rho, sigma):
+            xi = kappa - sigma * rho * u * 1j
+            return torch.sqrt(xi**2 + sigma**2 * u * (u + 1j))
+        d_partial_real = F.jacrev(lambda x,y: d_func(x,y).real, (0,1))(rho, sigma)
+        d_partial_imag = F.jacrev(lambda x,y: d_func(x,y).imag, (0,1))(rho, sigma)
+        dd_drho = (d_partial_real[0] + 1j * d_partial_imag[0]).squeeze(-1)
+        dd_dsigma = (d_partial_real[1] + 1j * d_partial_imag[1]).squeeze(-1)
         
-        A2.backward()
-        dA2_drho = rho.grad.clone()
-        rho.grad.zero_()
-        dA2_dsigma = sigma.grad.clone()
-        sigma.grad.zero_()
+        def A2_func(rho, sigma):
+            d = d_func(rho, sigma)
+            return d * torch.cosh(d * T / 2) + xi * torch.sinh(d * T / 2)
+        A2_partial_real = F.jacrev(lambda x,y: A2_func(x,y).real, (0,1))(rho, sigma)
+        A2_partial_imag = F.jacrev(lambda x,y: A2_func(x,y).imag, (0,1))(rho, sigma)
+        dA2_drho = (A2_partial_real[0] + 1j * A2_partial_imag[0]).squeeze(-1)
+        dA2_dsigma = (A2_partial_real[1] + 1j * A2_partial_imag[1]).squeeze(-1)
         
-        A.backward()
-        dA_drho = rho.grad.clone()
-        rho.grad.zero_()
-        dA_dsigma = sigma.grad.clone()
-        sigma.grad.zero_()
-        
-        B.backward()
-        dB_dkappa = kappa.grad.clone()
-        kappa.grad.zero_()
+        def A_func(rho, sigma):
+            A2 = A2_func(rho, sigma)
+            return A1 / A2
+        A_partial_real = F.jacrev(lambda x,y: A_func(x,y).real, (0,1))(rho, sigma)
+        A_partial_imag = F.jacrev(lambda x,y: A_func(x,y).imag, (0,1))(rho, sigma)
+        dA_drho = (A_partial_real[0] + 1j * A_partial_imag[0]).squeeze(-1)
+        dA_dsigma = (A_partial_real[1] + 1j * A_partial_imag[1]).squeeze(-1)
+            
+        def B_func(kappa):
+            xi = kappa - sigma * rho * u * 1j
+            d = torch.sqrt(xi**2 + sigma**2 * u * (u + 1j))
+            return torch.exp(torch.log(d) + (kappa - d) * T/ 2 - torch.log((d + xi) / 2 + (d - xi) * torch.exp(-d * T) / 2))
+        B_partial_real = F.jacrev(lambda x: B_func(x).real)(kappa)
+        B_partial_imag = F.jacrev(lambda x: B_func(x).imag)(kappa)
+        dB_dkappa =(B_partial_real + 1j * B_partial_imag).squeeze(-1)
         
         # Gradient vector of the characteristic function wrt [V0,theta,rho,kappa,sigma]
         h1 = -A
@@ -133,22 +133,22 @@ class HestonModel:
         h5 = -V0 * dA_dsigma - 4 * kappa * theta * D / sigma**3 + 2 * kappa * theta * (dd_dsigma - d * dA2_dsigma / A2) / sigma**2 / d + T * kappa * theta * rho * u * 1j / sigma**2
         grad_vec = torch.stack([h1, h2, h3, h4, h5], dim=2)
         
-        # Gauss-Legendre quadrature        
-        integrand = torch.real((K**(-u * 1j) / (u * 1j)).unsqueeze(-1) * char_func * grad_vec)
-        integrand = integrand[:2*num_nodes,:,:] - K.unsqueeze(-1) * integrand[2*num_nodes:,:,:]
-        integrand = w.reshape(-1,1,1) * 0.5 * u_max * (integrand[:num_nodes,:,:] + integrand[num_nodes:,:,:])
+        # Gauss-Legendre quadrature
+        integrand = torch.real((K**(-u * 1j) / (u * 1j)).unsqueeze(-1) * char_func.unsqueeze(-1) * grad_vec)
+        integrand = integrand[:num_nodes,:,:] - K.unsqueeze(-1) * integrand[num_nodes:,:,:]
+        integrand = w.reshape(-1,1,1) * (integrand[:num_nodes//2,:,:] + integrand[num_nodes//2:,:,:])
         integrand = torch.sum(integrand, dim=0) * torch.exp(-r * T.reshape(-1,1)) / np.pi
         
-        return integrand.T
+        return integrand
     
-    def train_params(self, K, T, S0, r):
+    def calibrate(self, K, T, S0, r):
         
         # Training method is based on the Levenberg-Marquardt method, but with the Jacobian calculated analytically
         # Paper is found at https://arxiv.org/pdf/1511.08718
         
         # K and T are lists of strike prices and corresponding times to maturity
-        self.market['K'] = torch.tensor([K]).to(device).reshape(1,-1)
-        self.market['T'] = torch.tensor([T]).to(device).reshape(1,-1)
+        self.market['K'] = torch.tensor(K).to(device).reshape(1,-1)
+        self.market['T'] = torch.tensor(T).to(device).reshape(1,-1)
         self.market['S0'] = torch.tensor([S0]).to(device)
         # Assumes no dividend. If there is, subtract from r for that asset. Since r is a scalar, it will have to be transformed into a tensor of the correct shape
         self.market['r'] = torch.tensor([r]).to(device)
@@ -156,14 +156,26 @@ class HestonModel:
         u = torch.tensor(x64).to(device).reshape(-1,1)
         w = torch.tensor(w64).to(device).reshape(-1,1)
         
+        # From the paper, their code uses 200 as an upper bound with 64 nodes
+        u_max = 200
+        u = torch.cat((u,-u), dim=0)
+        u = 0.5 * u_max * (u + 1) # Rescaling to quadrature domain
+        u = torch.cat((u-1j,u), dim=0)
+        
+        w = 0.5 * u_max * w
+        
+        self.quad['u'] = u
+        self.quad['w'] = w
+        
         # The paper used a tolerance of 1e-10. Default is 1e-8 which should be good enough
-        # They also used an initial damping factor equal to time to maturity, which is fucking huge; the default is 1e-3
+        # They also used an initial damping factor equal to time to maturity
         # Therefore I decided to use default. All other parameters as well
         # In particular, max_iter is 100, where in the paper the results are pretty good after less than 20
-        return lsq_lma(self.params,
+        return self.lsq_lma(self.params,
                        self.pricing,
                        self.jacobian,
-                       (self.market, u, w))
+                       (self.market, u ,w),
+                       tau=max(T))
         # This supposedly returns a list containing the parameters at each iteration
 
     def single_asset_path(self, u, dt, psic=1.5, gamma1=0.5, gamma2=0.5):
@@ -314,6 +326,112 @@ class HestonModel:
             pbar.close()
         
         return path
+
+    def lsq_lma(self,
+            p: torch.Tensor,
+            function, 
+            jac_function, 
+            args, 
+            ftol: float = 1e-8,
+            ptol: float = 1e-8,
+            gtol: float = 1e-8,
+            tau: float = 1e-3,
+            meth: str = 'lev',
+            rho1: float = .25, 
+            rho2: float = .75, 
+            bet: float = 2,
+            gam: float = 3,
+            max_iter: int = 100,
+        ):
+        """
+        Levenberg-Marquardt implementation for least-squares fitting of non-linear functions
         
+        :param p: initial value(s)
+        :param function: user-provided function which takes p (and additional arguments) as input
+        :param jac_fun: user-provided Jacobian function which takes p (and additional arguments) as input
+        :param args: optional arguments passed to function
+        :param ftol: relative change in cost function as stop condition
+        :param ptol: relative change in independant variables as stop condition
+        :param gtol: maximum gradient tolerance as stop condition
+        :param tau: factor to initialize damping parameter
+        :param meth: method which is default 'lev' for Levenberg and otherwise Marquardt
+        :param rho1: first gain factor threshold for damping parameter adjustment for Marquardt
+        :param rho2: second gain factor threshold for damping parameter adjustment for Marquardt
+        :param bet: multiplier for damping parameter adjustment for Marquardt
+        :param gam: divisor for damping parameter adjustment for Marquardt
+        :param max_iter: maximum number of iterations
+        :return: list of results
+        """
+        
+        # Ripped this off the torchimize pack because I couldn't get it to work
+
+        fun = lambda p: function(p, *args)
+        jac_fun = lambda p: jac_function(p, *args)
+        
+        f = fun(p)
+        j = jac_fun(p)
+        g = torch.matmul(j.T, f)
+        H = torch.matmul(j.T, j)
+        u = tau * torch.max(torch.diag(H))
+        v = 2
+        p_list = [p]
+        while len(p_list) < max_iter:
+            D = torch.eye(j.shape[1], device=j.device)
+            D *= 1 if meth == 'lev' else torch.max(torch.maximum(H.diagonal(), D.diagonal()))
+            try:
+                h = -torch.linalg.lstsq(H+u*D, g, rcond=None, driver=None)[0]
+            except:
+                print("Error encountered")
+                return p_list
+            f_h = fun(p+h)
+            rho_denom = torch.matmul(h.T, u*h-g)
+            rho_nom = torch.matmul(f.T, f) - torch.matmul(f_h.T, f_h)
+            rho = rho_nom / rho_denom if rho_denom > 0 else torch.inf if rho_nom > 0 else -torch.inf
+            if rho > 0:
+                p = p + h
+                j = jac_fun(p)
+                g = torch.matmul(j.T, fun(p))
+                H = torch.matmul(j.T, j)
+            p_list.append(p.clone())
+            f_prev = f.clone()
+            f = fun(p)
+            if meth == 'lev':
+                u, v = (u*torch.max(torch.tensor([1/3, 1-(2*rho-1)**3])), 2) if rho > 0 else (u*v, v*2)
+            else:
+                u = u*bet if rho < rho1 else u/gam if rho > rho2 else u
+
+            # stop conditions
+            gcon = max(abs(g)) < gtol
+            pcon = (h**2).sum()**.5 < ptol*(ptol + (p**2).sum()**.5)
+            fcon = ((f_prev-f)**2).sum() < ((ftol*f)**2).sum() if rho > 0 else False
+            if gcon or pcon or fcon:
+                break
+
+        return p_list       
+
+if __name__ == "__main__":
+    
+    # Calibration test
+    
+    karr = [0.9371, 0.8603, 0.8112, 0.7760, 0.7470, 0.7216, 0.6699, 0.6137,
+            0.9956, 0.9868, 0.9728, 0.9588, 0.9464, 0.9358, 0.9175, 0.9025,
+            1.0427, 1.0463, 1.0499, 1.0530, 1.0562, 1.0593, 1.0663, 1.0766,
+            1.2287, 1.2399, 1.2485, 1.2659, 1.2646, 1.2715, 1.2859, 1.3046,
+            1.3939, 1.4102, 1.4291, 1.4456, 1.4603, 1.4736, 1.5005, 1.5328]
+
+    tarr = [0.119047619047619,  0.238095238095238,	0.357142857142857,  0.476190476190476,	0.595238095238095,  0.714285714285714,  1.07142857142857,  1.42857142857143,
+            0.119047619047619,	0.238095238095238,  0.357142857142857,  0.476190476190476,  0.595238095238095,  0.714285714285714,  1.07142857142857,  1.42857142857143,
+            0.119047619047619, 	0.238095238095238,	0.357142857142857,	0.476190476190476,	0.595238095238095,	0.714285714285714,	1.07142857142857,  1.42857142857143,
+            0.119047619047619,	0.238095238095238,	0.357142857142857,	0.476190476190476,  0.595238095238095,	0.714285714285714,	1.07142857142857,  1.42857142857143,
+            0.119047619047619,	0.238095238095238,	0.357142857142857,	0.476190476190476,	0.595238095238095,	0.714285714285714,	1.07142857142857,  1.42857142857143]
+
+    S0 = 1.0
+    r = 0.02
+
+    model = HestonModel()
+    print(model.calibrate(karr, tarr, S0, r))
+    
+    # TODO #
+    # Simulation test
         
     
